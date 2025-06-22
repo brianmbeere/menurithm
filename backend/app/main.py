993a50d13922b1ae1174f28bb448fa4f6505221a
Brinjenga <briannjenga413@gmail.com>
2025-06-21 21:00:07 -0400

    backend port setting

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 from app.routes import menu, inventory, sales, dish
 from app.db.database import Base, engine
 from dotenv import load_dotenv
@@ -31,3 +32,7 @@ app.include_router(sales.router)
 app.include_router(dish.router)
 
 Base.metadata.create_all(bind=engine)
+
+
+port = int(os.environ.get("PORT", 8000))  # fallback to 8000 if not set
+uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
