14d6ab5bbf66e436e593408b0aaab56bc4fadb39
Brinjenga <briannjenga413@gmail.com>
2025-06-21 21:21:57 -0400

    port setup for both backend and frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173,  // for dev
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173,  // for preview
+  }
 })
-
